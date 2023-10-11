3ac85549f3200dba0e1c2e9df0394e3a92ed5f44
Lucas Parreiras <lucassouzaparreiras@hotmail.com>
2023-10-11 13:40:48 -0300

    Começando o desenvolvimento.

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    render() {
+        return (
+        <>
+            <h1>Main Page aqui!</h1>
+        </>
+        )
+    }
+}
+
+export default MainPage;
